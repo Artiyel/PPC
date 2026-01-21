c615ba0b3c182e5c1b4376fbfdf84a17fda34698
rainbowsdy <alexisrizza1@gmail.com>
2026-01-21 09:15:53 +0100

    commit

--- proie.py
+++ proie.py
@@ -1,6 +1,21 @@
-import random,signal 
+import random,signal,time
 
 def proie():
     energie=100
     Alive=True
-    while Alive
+    while Alive:
+        time.sleep(random.randint(0,5))
+        if energie > 80:
+            print("feed on grass (and more)")
+            #gagne de l'energie from eating grass
+            #reproduce
+            #perd 5 energie en plus (ils baisent fort)
+
+        elif energie > 60 :
+            print("just feed on grass")
+            #gagne de energie from eating grass
+        else :
+            print("se reposer")
+            #gagne de l'energie
+        #perd de l'énergie
+        
